44fa8c216949b9b0be2d4e6108e0acbfe4c90ad0
Subhajit Sahu <wolfram77@gmail.com>
2021-03-03 17:24:00 +0530

    :ringed_planet: w/ mtx fetch script

--- install.sh
+++ install.sh
@@ -2,19 +2,18 @@
 
 # Download nvGraph
 cd ~
-git clone https://github.com/rapidsai/nvgraph
-cd ~/nvgraph
-git submodule update --init --recursive
+git clone --recurse-submodules https://github.com/rapidsai/nvgraph
 
 # Install nvGraph
-export CUDA_ROOT="/usr/local/cuda" && ./build.sh
+export CUDA_ROOT="/usr/local/cuda"
+cd ~/nvgraph && ./build.sh
 
 
 # Build and Install the C/C++ CUDA components
-mkdir -p ~/nvgraph/cpp/build
-cd ~/nvgraph/cpp/build
-cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
-make -j && make install
+# mkdir -p ~/nvgraph/cpp/build
+# cd ~/nvgraph/cpp/build
+# cmake .. -DTHRUST_IGNORE_DEPRECATED_CPP_DIALECT=1 -DTHRUST_IGNORE_CUB_VERSION_CHECK=1 -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
+# make -j && make install
 
 
 # C++ stand alone tests
